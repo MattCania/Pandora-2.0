e8ef0d9a37de4eb90a837b2fee8c1226462ee377
Matthew Cania <139668141+MattCania@users.noreply.github.com>
2025-01-02 23:27:12 +0800

    remote commit

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import LandingPage from './pages/Landing'
+import './App.css'
+
+function App() {
+  return (
+    <>
+		<h1 className='text-xl font-bold'>Hello World</h1>
+      {/* <LandingPage/> */}
+    </>
+  )
+}
+
+export default App
---
+++ Frontend/src/pages/Landing.jsx
@@ -0,0 +1,10 @@
+
+function LandingPage() {
+
+	return (
+		<h1 className="text-4xl">Hello World</h1>
+	)
+
+}
+
+export default LandingPage
