8e82e56ae294a5b1e95f75a691ebe317eb601d7d
Matthew <matthewgab24@gmail.com>
2025-01-07 18:38:46 +0800

    minor frontend changes

--- Frontend/src/partials/MainHeader.jsx
+++ Frontend/src/partials/MainHeader.jsx
@@ -1,16 +1,31 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 import Menu from './Menu'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 
 
-export default function MainHeader () {
+export default function MainHeader() {
 	const [menuOpen, setMenu] = useState(false)
 
+	const menuRef = useRef(null);
 	const toggleMenu = () => {
-		setMenu (menuOpen => !menuOpen)
+		setMenu(menuOpen => !menuOpen)
 	}
+	useEffect(() => {
+		const handleClickOutside = (event) => {
+			if (menuRef.current && !menuRef.current.contains(event.target)) {
+				setMenu(false);
+			}
+		};
 
+		if (menuOpen) {
+			document.addEventListener("mousedown", handleClickOutside);
+		}
+
+		return () => {
+			document.removeEventListener("mousedown", handleClickOutside);
+		};
+	}, [menuOpen]);
 
 
 	return (
@@ -19,26 +34,29 @@ export default function MainHeader () {
 				<h1 className='text-md font-medium text-white' >
 					Search:
 				</h1>
-				<input 
-				className='rounded-md border-none placeholder:italic outline-none px-2 h-8 min-w-72 w-auto'
-				type="search" placeholder='Pandora Invoice' />
+				<input
+					className='rounded-md border-none placeholder:italic outline-none px-2 h-8 min-w-72 w-auto'
+					type="search" placeholder='Pandora Invoice' />
 			</div>
 
 			<div className='flex justify-end items-center w-1/2' >
 				<h1 className='w-fit border-x-gray-500 border-x text-md text-white px-8 transition-all hover:px-16'>
 					Matt Organization
 				</h1>
-				<button 
-				className='flex justify-center items-center text-white p-4 mx-4 text-xl font-medium h-8 w-8 rounded-md border border-transparent transition-all hover:border hover:border-gray-500 '
-				onClick={toggleMenu}
-	
+				<button
+					className='flex justify-center items-center text-white p-4 mx-4 text-xl font-medium h-8 w-8 rounded-md border border-transparent transition-all hover:border hover:border-gray-500 '
+					onClick={toggleMenu}
+
 				>
-					<FontAwesomeIcon icon={faBars}/>
+					<FontAwesomeIcon icon={faBars} />
 				</button>
 			</div>
 
-			{ menuOpen &&
-				<Menu/>
+			{menuOpen &&
+				<div ref={menuRef}>
+					
+				<Menu />
+				</div>
 			}
 		</header>
 
