0b8a42bdea459710f73ec7dc72d67c58ff5192bd
Matthew Cania <matthewgab24@gmail.com>
2025-02-04 15:11:25 +0800

    reset frontend, swapped to Typescript

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div
+      className='h-screen w-screen flex justify-center items-center'
+    >
+      <h1
+        className='text-5xl text-blue-500'
+      >
+        Hello
+      </h1>
+
+    </div>
+  )
+}
+
+export default App
--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,16 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    port: 3173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-      },
-    },
+    port: 1137
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
 })
