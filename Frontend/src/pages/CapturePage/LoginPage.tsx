40927c48b9156ed750e9555010414d1cebbb2599
Matthew Cania <matthewgab24@gmail.com>
2025-02-07 21:17:42 +0800

    finished (base) for login page

--- Frontend/src/pages/CapturePage/LoginPage.tsx
+++ Frontend/src/pages/CapturePage/LoginPage.tsx
@@ -1,49 +1,89 @@
+import Waves from "../../components/Waves";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { Link } from "react-router-dom";
+
 export default function LoginPage() {
   return (
     <section
-    className='flex flex-col justify-center items-center w-full overflow-x-hidden overflow-y-auto'
-    style={{
-      minHeight: "100vh",
-      height: "auto",
-    }}
+      className='flex flex-col justify-center items-center w-full overflow-x-hidden overflow-y-auto gap-4'
+      style={{
+        minHeight: "100vh",
+        height: "auto",
+      }}
     >
+      <Waves />
 
-      <div
-        
-        className='flex flex-col justify-center items-center w-full lg:w-3/4 h-3/4 border'
-      >
-        <h1
-          className="flex w-full h-16 justify-center items-center text-zinc-950 text-4xl font-medium p-12"
-        >
+      <div className='flex flex-col justify-center items-center w-full lg:w-1/3 h-1/2 rounded-2xl bg-white shadow-xl shadow-zinc-950'>
+        <h1 className='flex w-full h-16 justify-center items-center text-emerald-950 text-4xl font-medium p-4'>
           Sign In
         </h1>
-      <form 
-        className="flex flex-col justify-center items-center w-1/2 h-1/2 "
-        action=''
-      >
-        <label htmlFor='email'>
-			Email:
-		</label>
+        <p
+          className="flex justify-center items-center w-full h-auto gap-2"
+        >
+          Create an account?
+          <Link 
+          className="underline font-medium hover:text-emerald-500 transition-all duration-500"
+          to=""
+          >
+            Sign In
+          </Link>
+        </p>
+        <form
+          className='flex flex-col justify-start items-center w-1/2 h-1/2 py-4 '
+          action=''
+        >
+          <label
+            className='flex w-full justify-start text-emerald-950 font-medium items-center'
+            htmlFor='email'
+          >
+            Email:
+          </label>
 
-        <input
-          className="flex w-3/4 border"
-          id='email'
-          name='email'
-          type='email'
-        />
+          <input
+            className='flex w-full h-8 rounded-md outline-0 px-2 border bg-emerald-200 font-medium'
+            id='email'
+            name='email'
+            type='email'
+          />
 
-        <label htmlFor='password'>
-			Password
-		</label>
+          <label
+            className='flex w-full justify-start text-emerald-950 font-medium items-center'
+            htmlFor='password'
+          >
+            Password:
+          </label>
 
-        <input
-          id='password'
-          name='password'
-          type={`password`}
-        />
-      </form>
-      
+          <input
+            className='flex w-full h-8 rounded-md outline-0 px-2 border bg-emerald-200 font-medium'
+            id='password'
+            name='password'
+            type='password'
+          />
+
+          <input
+            className='flex justify-center items-center w-32 h-16 m-8 bg-emerald-900 text-emerald-200 font-medium rounded-md hover:bg-emerald-400 hover:text-emerald-950 transition-all duration-500 cursor-pointer '
+            type='submit'
+          />
+        </form>
+        <div className='flex justify-center items-center w-full h-auto rounded-2xl bg-white gap-8'>
+        <Link
+          className='text-zinc-900 text-3xl hover:text-emerald-600 transition-all duration-500'
+          to=''
+        >
+          <FontAwesomeIcon icon={faGoogle} />
+        </Link>
+        <Link
+          className='text-zinc-900 text-3xl hover:text-emerald-600 transition-all duration-500'
+          to=''
+        >
+          <FontAwesomeIcon icon={faEnvelope} />
+        </Link>
       </div>
+      </div>
+
+      
     </section>
   );
 }
